223e243dd0dcb42e9101c044ff801ff3ab67ec3a
eccentricnode <shival.bigman@gmail.com>
2021-08-07 19:24:16 -0700

    incremental checkin

--- js/site.js
+++ js/site.js
@@ -47,7 +47,7 @@ function displayNumbers(numbers) {
 
     for (let i = 0; i < numbers.length; i++) {
         
-        let number = numbers[i];
+        let number = numbers[];
         if (number % 2 == 0) {
             className="even"
         } else {
